7cc103adbf5b893ca9c4726fa2862b21a0ac6221
johinthampi <johinthampi@gmail.com>
2024-08-20 00:26:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+//import json-server
+const jsonServer = require('json-server')
+
+//create server using create() method
+const mediaPlayerServer = jsonServer.create()
+
+//create a path/ route to db.json file
+const router = jsonServer.router('db.json')
+
+//create a middle ware
+const middleware = jsonServer.defaults();
+
+//add middle ware and router to mediaplayerServer
+
+mediaPlayerServer.use(middleware);
+mediaPlayerServer.use(router);
+
+const PORT = 4600;
+
+//run the mediaPlayerServer
+mediaPlayerServer.listen(PORT,()=>{
+    console.log(`mediaplayer server running on PORT ${PORT}`);
+})
